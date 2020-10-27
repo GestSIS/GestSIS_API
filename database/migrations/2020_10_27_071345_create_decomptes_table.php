61a53abddb84f3151031998ff587c9235a920660
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-10-27 09:03:04 +0100

    création migrations

---
+++ database/migrations/2020_10_27_071345_create_decomptes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDecomptesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decomptes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->bigInteger('exercice_comptable_id')->unsigned()->nullable();
+            $table->foreign('exercice_comptable_id')->references('id')->on('exercice_comptables');
+
+            $table->string('designation');
+            $table->integer('deduction');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decomptes');
+    }
+}
