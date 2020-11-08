e8d46c3bc1ca361978daa023ed4aeb482f5540dd
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-11-08 14:39:54 +0100

    ajout tests

---
+++ tests/Feature/DecompteTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeompteTest extends TestCase
+{
+    /**
+     * création d'un décompte.
+     *
+     * @return void
+     */
+    public function testDecompteCreation()
+    {
+        $response = $this->json('GET', "api/v2/decompte/create");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'designation',
+                    'exercice_comptable_id',
+                    'deduction'
+                ]
+            ]);
+    }
+
+    /**
+     * get all décomptes.
+     *
+     * @return void
+     */
+    public function testDecompteAll()
+    {
+        $response = $this->json('GET', "api/v2/decompte/");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'designation',
+                        'exercice_comptable_id',
+                        'deduction'
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * get all décomptes.
+     *
+     * @return void
+     */
+    public function testDecompteAnneeComptable()
+    {
+        $response = $this->json('GET', "api/v2/decompte/exerciceComptable/3");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'designation',
+                        'exercice_comptable_id',
+                        'deduction'
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * get un décompte.
+     *
+     * @return void
+     */
+    public function testDecompte()
+    {
+        $response = $this->json('GET', "api/v2/decompte/1");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'designation',
+                    'exercice_comptable_id',
+                    'deduction'
+                ]
+            ]);
+    }
+}
--- tests/Feature/PaiementsTest.php
+++ tests/Feature/PaiementsTest.php
@@ -8,15 +8,114 @@ use Tests\TestCase;
 
 class PaiementsTest extends TestCase
 {
+
     /**
-     * A basic feature test example.
+     * get all paiements.
      *
      * @return void
      */
-    public function testExample()
+    public function testPaiementAll()
     {
-        $response = $this->get('/');
+        $response = $this->json('GET', "api/v2/paiement/");
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'decompte_id',
+                        'solde',
+                        'indeminte',
+                        'frais',
+                        'amende',
+                        'avs',
+                        'total',
+                        'sapeur_id',
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * get  paiements.
+     *
+     * @return void
+     */
+    public function testPaiement()
+    {
+        $response = $this->json('GET', "api/v2/paiement/1");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'decompte_id',
+                    'solde',
+                    'indeminte',
+                    'frais',
+                    'amende',
+                    'avs',
+                    'total',
+                    'sapeur_id',
+                ]
+            ]);
+    }
+
+    /**
+     * get all paiements for annee comptable.
+     *
+     * @return void
+     */
+    public function testPaiementAnnee()
+    {
+        $response = $this->json('GET', "api/v2/paiement/exerciceComptable/4");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'decompte_id',
+                        'solde',
+                        'indeminte',
+                        'frais',
+                        'amende',
+                        'avs',
+                        'total',
+                        'sapeur_id',
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * get all paiements for decompte.
+     *
+     * @return void
+     */
+    public function testPaiementDecompte()
+    {
+        $response = $this->json('GET', "api/v2/paiement/decompte/1");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'decompte_id',
+                        'solde',
+                        'indeminte',
+                        'frais',
+                        'amende',
+                        'avs',
+                        'total',
+                        'sapeur_id',
+                    ]
+                ]
+            ]);
     }
 }
