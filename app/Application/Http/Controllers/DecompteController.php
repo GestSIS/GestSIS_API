51897e492681d3d9bd84a4420286c7c0d93cac62
Jonatan Baumgartner <jonatan.baumgartner@he-arc.ch>
2020-11-22 12:23:42 +0100

    création PaiementBusiness

--- app/Application/Http/Controllers/DecompteController.php
+++ app/Application/Http/Controllers/DecompteController.php
@@ -5,6 +5,7 @@ namespace App\Application\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Domaine\API\ComptabiliteService;
+use App\Domaine\Business\PaiementBusiness;
 use App\Infrastructure\Models\Decompte;
 use App\Infrastructure\Models\Ecriture;
 use App\Infrastructure\Models\Paiement;
@@ -39,108 +40,7 @@ class DecompteController extends Controller
             'minimumSoldeImposable' => 'numeric'
         ]);
 
-        $decompte = new Decompte();
-        $decompte->designation = $data['designation'];
-        $decompte->exercice_comptable_id = $data['exerciceComptableId'];
-        $decompte->deduction = $data['deduction'];
-        $decompte->avsTotal = 0;
-        $decompte->acTotal = 0;
-        $decompte->save();
-
-        $ecritures = Ecriture::where('exercice_comptable_id', $data['exerciceComptableId'])->get();
-
-        $totaux = array();
-        //faire les totaux par sapeurs
-        foreach ($ecritures as $ecriture) {
-            //ne pas ajouter une écriture déja payé
-            if ($ecriture->decompte_id == null) {
-                if (!array_key_exists($ecriture->sapeur_id, $totaux)) {
-                    $totaux[$ecriture->sapeur_id] = array(
-                        "solde" => 0.0,
-                        "indemnite" => 0.0,
-                        "frais" => 0.0,
-                        "amende" => 0.0,
-                        "avs" => 0.0,
-                        "total" => 0.0,
-                        "soldeTotal" => 0.0,
-                        "indemniteTotal" => 0.0,
-                        "avsTotal" => 0.0
-                    );
-                }
-                $totaux[$ecriture->sapeur_id]['solde'] += $ecriture->solde;
-                $totaux[$ecriture->sapeur_id]['indemnite'] += $ecriture->indemnite;
-                $totaux[$ecriture->sapeur_id]['frais'] += $ecriture->frais;
-                //pas encore présent dans écriture
-                //$totaux[$ecriture->sapeur_id]['amende']+=$ecriture->amende;
-
-                $ecriture->date_paiement = date('Y-m-d');
-                $ecriture->decompte_id = $decompte->id;
-                $ecriture->save();
-
-            }
-        }
-
-        //déductions
-        if ($data['deduction']) {
-            $taux = $data['taux_ac'] + $data['taux_avs'];
-            //vérifie si autre décompte sans déduction
-            foreach (Decompte::where('exercice_comptable_id', $data['exerciceComptableId'])->with('paiements')->get() as $d) {
-                foreach ($d->paiements as $p) {
-                    if (!array_key_exists($p->sapeur_id, $totaux)) {
-                        $totaux[$p->sapeur_id] = array(
-                            "solde" => 0.0,
-                            "indemnite" => 0.0,
-                            "frais" => 0.0,
-                            "amende" => 0.0,
-                            "avs" => 0.0,
-                            "total" => 0.0,
-                            "soldeTotal" => 0.0,
-                            "indemniteTotal" => 0.0,
-                            "avsTotal" => 0.0
-                        );
-                    }
-                    $totaux[$p->sapeur_id]['soldeTotal'] += $p->solde;
-                    $totaux[$p->sapeur_id]['indemniteTotal'] += $p->indemine;
-                    $totaux[$p->sapeur_id]['avsTotal'] += $p->avs;
-                }
-            }
-            foreach ($totaux as $key => $total) {
-                $solde_imposable = max($total['solde'] + $total['soldeTotal'] - $data['minimumSoldeImposable'], 0.0);
-                $total_imposable = $solde_imposable + $total['indemnite'] + $total['indemniteTotal'];
-                
-            
-                //TODO ou si sapeur fait la demande
-                if ($total_imposable > $data['minimumImposableAVSAC']) {
-                    $totaux[$key]['avs'] = ($total_imposable * $taux)-$total['avsTotal'];
-                    $decompte->avsTotal += ($total_imposable * $data['taux_avs'])-(($total['avsTotal']/$taux)*$data['taux_avs']);
-                    $decompte->acTotal += ($total_imposable * $data['taux_ac'])-(($total['avsTotal']/$taux)*$data['taux_ac']);
-                }
-            }
-            $decompte->save();
-        }
-
-        //total à payer
-        foreach ($totaux as $key => $total) {
-            $totaux[$key]['total'] = $total['solde'] + $total['indemnite'] + $total['frais'] - $total['avs'];
-        }
-
-        $paiements = array();
-        //création paiements
-        foreach ($totaux as $key => $total) {
-            $paiements[] = [
-                'decompte_id' => $decompte->id,
-                'solde' => $total['solde'],
-                'indemnite' => $total['indemnite'],
-                'frais' => $total['frais'],
-                'amende' => $total['amende'],
-                'avs' => $total['avs'],
-                'total' => $total['total'],
-                'sapeur_id' => $key
-            ];
-        }
-        Paiement::insert($paiements);
-
-        return response()->json(['data' => $decompte]);
+        return response()->json(['data' => PaiementBusiness::creer($data)]);
     }
 
     /**
