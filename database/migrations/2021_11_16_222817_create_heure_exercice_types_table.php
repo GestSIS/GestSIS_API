74f82ce572789d338d03ecf5383e9a10342b3917
bastien wermeille <bastien.wermeille@gmail.com>
2021-11-28 16:18:07 +0100

    Creation tables heure sup.

---
+++ database/migrations/2021_11_16_222817_create_heure_exercice_types_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHeureExerciceTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heure_exercice_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('designation');
+            $table->unsignedDecimal('montant');
+
+            $table->unsignedBigInteger('compte_id');
+            $table->foreign('compte_id')->references('id')->on('comptes');
+
+            $table->unsignedBigInteger('ecriture_categorie_id');
+            $table->foreign('ecriture_categorie_id')->references('id')->on('ecriture_categories');
+
+            $table->unsignedBigInteger('type_unite_id');
+            $table->foreign('type_unite_id')->references('id')->on('type_unites');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heure_exercice_types');
+    }
+}
---
+++ database/migrations/2021_11_16_222832_create_heure_exercices_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHeureExercicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heure_exercices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->unsignedDecimal('quantite');
+
+            $table->string('designation');
+            $table->unsignedDecimal('montant');
+
+            $table->unsignedBigInteger('exercice_id');
+            $table->foreign('exercice_id')->references('id')->on('exercices');
+
+            $table->unsignedBigInteger('sapeur_id');
+            $table->foreign('sapeur_id')->references('id')->on('sapeurs');
+
+            $table->unsignedBigInteger('type_unite_id');
+            $table->foreign('type_unite_id')->references('id')->on('type_unites');
+
+            $table->unsignedBigInteger('heure_exercice_type_id')->nullable();
+            $table->foreign('heure_exercice_type_id')->references('id')->on('heure_exercice_types')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heure_exercices');
+    }
+}
