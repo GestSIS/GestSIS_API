748ce81f67a5bd5349c8d3bedfab0deb2e8cb1d3
bastien wermeille <bastien.wermeille@gmail.com>
2021-12-05 23:17:17 +0100

    cast update pour presences

--- app/Infrastructure/Models/ExerciceSapeur.php
+++ app/Infrastructure/Models/ExerciceSapeur.php
@@ -9,7 +9,7 @@ class ExerciceSapeur extends Model
 {
     protected $table = 'exercice_sapeur';
     protected $fillable = ['convoque', 'present', 'amende', 'remplace', 'excuse_type_id'];
-    protected $casts = ['convoque' => 'boolean', 'amende' => 'boolean', 'remplace' => 'boolean', 'excuse_type_id' => 'integer'];
+    protected $casts = ['convoque' => 'integer', 'amende' => 'integer', 'remplace' => 'integer', 'excuse_type_id' => 'integer'];
 
     use HasFactory;
 }
