2388fab335c050e730df6ae6b147730d7ce007aa
bastien wermeille <bastien.wermeille@gmail.com>
2022-02-07 00:17:18 +0100

    Enhance data return by api route effectif

--- app/Domaine/API/SapeurService.php
+++ app/Domaine/API/SapeurService.php
@@ -29,7 +29,7 @@ class SapeurService
     {
         return Sapeur::with('telephones', 'permis', 'fonctions', 'groupes')
             ->where('actif', '=', '1')
-            ->get(['id', 'nom', 'prenom', 'email', 'date_naissance', 'fonction_id', 'grade_id', 'civilite_id'])->toArray();
+            ->get(['id', 'nom', 'prenom', 'email', 'rue', 'no_rue', 'date_naissance', 'fonction_id', 'grade_id', 'civilite_id', 'localite_id'])->toArray();
     }
 
     public function getSapeurDetailsById($sapeurid)
