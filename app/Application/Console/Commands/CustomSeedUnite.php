9d15fd27e7bac771dcfa150d552f6f4ecf1b90ae
bastien wermeille <bastien.wermeille@gmail.com>
2022-02-10 11:30:28 +0100

    Custom seed for unité mois

---
+++ app/Application/Console/Commands/CustomSeedUnite.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Application\Console\Commands;
+
+use App\Infrastructure\Models\TypeUnite;
+use Illuminate\Console\Command;
+
+class CustomSeedUnite extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seed-unites:dbs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $dbs = explode(",", env('DB_LISTE', true));
+        foreach ($dbs as $db) {
+            printf("DATABASE " . $db . "\n");
+            printf("Seed unites\n");
+            TypeUnite::on($db)->insert(['id' => '7', 'comptable' => True, 'unite' => 'mois', 'abreviation' => 'm']);
+            printf("\n");
+        }
+        printf("Migrating done");
+        return 0;
+    }
+}
