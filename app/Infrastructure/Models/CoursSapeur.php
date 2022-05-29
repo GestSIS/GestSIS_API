721f17cf8c22bc7f32800eb58dce828d002c2c87
bastien wermeille <bastien.wermeille@gmail.com>
2022-05-29 12:21:34 +0200

    Ajout durée cours

--- app/Infrastructure/Models/CoursSapeur.php
+++ app/Infrastructure/Models/CoursSapeur.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class CoursSapeur extends Model
 {
-    protected $fillable = ['date', 'localite_id'];
+    protected $fillable = ['date', 'localite_id', 'duree'];
     protected $casts = [
-        'localite_id' => 'integer'
+        'localite_id' => 'integer', 'duree' => 'decimal:2'
     ];
     protected $table = 'cours_sapeur';
 }
---
+++ database/migrations/2022_05_29_114917_update_cours_sapeur_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCoursSapeurTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cours_sapeur', function (Blueprint $table) {
+            $table->smallInteger('duree')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cours_sapeur', function (Blueprint $table) {
+            $table->removeColumn('duree');
+        });
+    }
+}
