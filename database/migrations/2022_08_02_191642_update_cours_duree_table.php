d8cf351e729fbff9906bdc5a2530edec455d737c
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-08-02 19:32:30 +0200

    Fix incorrect colonne type pour duree cours

---
+++ database/migrations/2022_08_02_191642_update_cours_duree_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCoursDureeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cours', function (Blueprint $table) {
+            $table->decimal('duree')->change();
+        });
+        Schema::table('cours_sapeur', function (Blueprint $table) {
+            $table->decimal('duree')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cours', function (Blueprint $table) {
+            $table->smallInteger('duree')->change();
+        });
+        Schema::table('cours_sapeur', function (Blueprint $table) {
+            $table->smallInteger('duree')->change();
+        });
+    }
+}
