34ee19382941e3ad8e8faff078997a1da48d6c2b
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-08-07 00:28:38 +0200

    Début alarme API pour link sapeurs

--- app/Domaine/API/AlarmeService.php
+++ app/Domaine/API/AlarmeService.php
@@ -6,7 +6,9 @@ namespace App\Domaine\API;
 use App\Domaine\Business\InterventionBusiness;
 use App\Domaine\Exceptions\ArrayException;
 use App\Domaine\SPI\InterventionRepository;
+use App\Infrastructure\Models\Sapeur;
 use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class AlarmeService
@@ -29,14 +31,48 @@ class AlarmeService
             $response = Http::withHeaders([
                 'Sis-Id' => $sisKey,
                 'Authorization' => 'Bearer ' . $token
-            ])->acceptJson()->timeout(3)->get(config('gestsis.alarm_url', '') . '/api/v1/alarm');
+            ])
+                ->acceptJson()
+                ->timeout(3)
+                ->get(config('gestsis.alarm_url', '') . '/api/v1/alarm', ['force_update' => $force]);
 
             if ($response->successful()) {
                 $alarmes = $response->object();
             }
 
-            // TODO: Enhance response
+            // Enhance response -> Se base uniquement sur les sapeurs actifs
+            $sapeurs = Sapeur::with('telephones')->where('actif', '=', 1)->get(['nom', 'prenom', 'suffixe', 'id']);
 
+            $indexedSapeursByNomPrenom = [];
+            foreach ($sapeurs as $sapeur) {
+                // TODO: Manage homonymes
+                $key = strtolower($sapeur['nom'] . " " . $sapeur['prenom']);
+                $indexedSapeursByNomPrenom[$key] = $sapeur;
+            }
+
+            $alarmes = array_map(function ($alarme) use ($indexedSapeursByNomPrenom) {
+                // Idée, simplement extraires les sapeurs sous leur forme que l'on peut rencontrer
+                $tmp = [];
+                $missing = [];
+
+                // Method 1 en utilisant nom et prénom
+                foreach ($alarme->firefighters as $f) {
+                    $nomPrenom = strtolower($f->fullname);
+                    if (array_key_exists($nomPrenom, $indexedSapeursByNomPrenom)) {
+                        $f->id = $indexedSapeursByNomPrenom[$nomPrenom]->id;
+                        array_push($tmp, $f);
+                    } else {
+                        array_push($missing, $f);
+                    }
+                }
+
+                // Method 2 en utilisant les numéros de téléphones
+                // TODO
+
+                return $alarme;
+            }, $alarmes);
+
+            return $alarmes;
         } catch (Exception $e) {
             dd($e);
             throw new ArrayException([], "Une erreur est survenue lors de la récupération des alarmes");
