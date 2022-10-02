dcf15b20fc1cc1feea10b628e6c7281623477454
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-10-02 23:50:37 +0200

    Fix supprimerConvocations + updates dependencies

--- app/Domaine/Business/ExerciceBusiness.php
+++ app/Domaine/Business/ExerciceBusiness.php
@@ -392,12 +392,14 @@ class ExerciceBusiness
 
     public function supprimerConvocations($sapeurId, $exerciceSapeursIds)
     {
-        throw new ArrayException([], 'Fonctionalités désactivée temporairement, réessayer plus tard !');
-        // TODO: Check pour chaque exercice s'il est possible de supprimer la convocation
-        // et donc que l'exercice n'est pas déjà imputé
         $this->repository->supprimerConvocations($sapeurId, $exerciceSapeursIds);
         HeureExercice::whereIn('id', $exerciceSapeursIds)
             ->where('sapeur_id', $sapeurId)
+            ->whereHas(["exercice" => function ($q) {
+                // Check pour chaque exercice s'il est possible de supprimer la convocation et donc que l'exercice n'est pas déjà imputé
+                $q->where('statut', '>', self::EXERCICE_STATUT_ANNULE)
+                    ->where('statut', '<=', self::EXERCICE_STATUT_SAISI);
+            }])
             ->delete();
         return true;
     }
