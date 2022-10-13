71b8fcd60b4ff324e2f50387afa117043bb4e7ba
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-10-13 11:05:15 +0200

    Mutations pour mat perso

--- database/migrations/2022_08_23_202549_module_materiel.php
+++ database/migrations/2022_08_23_202549_module_materiel.php
@@ -23,13 +23,33 @@ class ModuleMateriel extends Migration
             $table->foreign('materiel_categorie_id')->references('id')->on('materiel_categories');
         });
 
-        Schema::create('materiel_types', function (Blueprint $table) {
+        Schema::create('materiel_inventaire', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
 
-            $table->string('designation');
-            $table->boolean('attribuable');
-            $table->boolean('nominal');
+            $table->string('taille');
+            $table->unsignedInteger('quantite');
+            $table->string('remarque');
+
+            $table->bigInteger('materiel_categorie_id')->unsigned()->nullable();
+            $table->foreign('materiel_categorie_id')->references('id')->on('materiel_categories');
+        });
+
+        Schema::create('materiel_personnels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('taille');
+            $table->string('remarque');
+
+            $table->dateTime('attribution');
+            $table->dateTime('retour');
+
+            $table->bigInteger('sapeur_id')->unsigned()->unique();
+            $table->foreign('sapeur_id')->references('id')->on('sapeurs');
+
+            $table->string('materiel_type');
+            $table->bigInteger('materiel_id')->unsigned()->nullable();
 
             $table->bigInteger('materiel_categorie_id')->unsigned()->nullable();
             $table->foreign('materiel_categorie_id')->references('id')->on('materiel_categories');
@@ -40,52 +60,28 @@ class ModuleMateriel extends Migration
             $table->timestamps();
 
             $table->string('uuid')->unique();
+            $table->string('numero');
 
-            $table->string('taille');
             $table->dateTime('achat');
             $table->string('remarque');
-
-            $table->bigInteger('materiel_type_id')->unsigned()->unique();
-            $table->foreign('materiel_type_id')->references('id')->on('materiel_types');
         });
 
-        Schema::create('materiel_nominal_attributions', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-
-            $table->dateTime('attribution');
-            $table->dateTime('retour');
-            $table->string('remarque');
-
-            $table->bigInteger('sapeur_id')->unsigned()->unique();
-            $table->foreign('sapeur_id')->references('id')->on('sapeurs');
-
-            $table->bigInteger('materiel_nominal_id')->unsigned()->unique();
-            $table->foreign('materiel_nominal_id')->references('id')->on('materiel_nominals');
-        });
-
-        Schema::create('materiel_attributions', function (Blueprint $table) {
+        Schema::create('materiel_indiscernables', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
 
             $table->unsignedInteger('quantite');
-            $table->string('taille');
-            $table->string('remarque');
-            $table->dateTime('attribution');
-            $table->dateTime('retour');
-
-            $table->bigInteger('sapeur_id')->unsigned()->unique();
-            $table->foreign('sapeur_id')->references('id')->on('sapeurs');
-
-            $table->bigInteger('materiel_type_id')->unsigned()->unique();
-            $table->foreign('materiel_type_id')->references('id')->on('materiel_types');
         });
 
         Schema::create('materiel_event_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
 
-            $table->string('designation');
+            $table->string('nom');
+            $table->string('description');
+
+            // Propripétés
+            $table->boolean('validable');
         });
 
         Schema::create('materiel_events', function (Blueprint $table) {
@@ -93,7 +89,8 @@ class ModuleMateriel extends Migration
             $table->timestamps();
 
             $table->dateTime('date');
-            $table->dateTime('remarque');
+            $table->string('remarque');
+            $table->boolean('succes')->default(true);
 
             $table->bigInteger('materiel_nominal_id')->unsigned()->unique();
             $table->foreign('materiel_nominal_id')->references('id')->on('materiel_nominals');
@@ -106,11 +103,44 @@ class ModuleMateriel extends Migration
             $table->bigIncrements('id');
             $table->timestamps();
 
-            $table->bigInteger('materiel_type_id')->unsigned()->unique();
-            $table->foreign('materiel_type_id')->references('id')->on('materiel_types');
+            $table->bigInteger('materiel_categorie_id')->unsigned()->nullable();
+            $table->foreign('materiel_categorie_id')->references('id')->on('materiel_categories');
 
             $table->bigInteger('materiel_event_type_id')->unsigned()->unique();
             $table->foreign('materiel_event_type_id')->references('id')->on('materiel_event_types');
+        });
+
+        Schema::create('materiel_alert_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->dateTime('titre');
+            $table->string('description');
+
+            $table->integer('seuil_min');
+            $table->boolean('dernier');
+        });
+
+        Schema::create('materiel_alert_type_pour_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->bigInteger('materiel_alert_type_id')->unsigned()->unique();
+            $table->foreign('materiel_alert_type_id')->references('id')->on('materiel_alert_types');
+
+            $table->bigInteger('materiel_event_type_id')->unsigned()->unique();
+            $table->foreign('materiel_event_type_id')->references('id')->on('materiel_event_types');
+        });
+
+        Schema::create('materiel_alerts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->dateTime('titre');
+            $table->string('description');
+
+            $table->bigInteger('materiel_nominal_id')->unsigned()->unique();
+            $table->foreign('materiel_nominal_id')->references('id')->on('materiel_nominals');
         });
     }
 
@@ -121,12 +151,16 @@ class ModuleMateriel extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('materiel_alert_type_pour_events');
+        Schema::dropIfExists('materiel_alert_types');
         Schema::dropIfExists('materiel_event_pour_types');
         Schema::dropIfExists('materiel_events');
         Schema::dropIfExists('materiel_event_types');
-        Schema::dropIfExists('materiel_attributions');
+        Schema::dropIfExists('materiel_indiscernables');
+        Schema::dropIfExists('materiel_alerts');
         Schema::dropIfExists('materiel_nominals');
-        Schema::dropIfExists('materiel_types');
+        Schema::dropIfExists('materiel_personnels');
+        Schema::dropIfExists('materiel_inventaire');
         Schema::dropIfExists('materiel_categories');
     }
 }
