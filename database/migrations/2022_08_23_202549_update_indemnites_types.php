921d2d6eeb7399de4098727c74c1b3c7af9fb185
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-11-04 12:47:53 +0100

    Fix migrations

--- database/migrations/2022_08_23_202549_update_indemnites_types.php
+++ database/migrations/2022_08_23_202549_update_indemnites_types.php
@@ -14,10 +14,12 @@ class UpdateIndemnitesTypes extends Migration
     public function up()
     {
         Schema::table('indemnite_exercice_fonctions', function (Blueprint $table) {
+            $table->dropForeign(['indemnite_exe_id']);
             $table->foreign('indemnite_exe_id')->references('id')->on('indemnite_exercice_types')->onDelete('cascade');
         });
 
         Schema::table('indemnite_intervention_fonctions', function (Blueprint $table) {
+            $table->dropForeign(['indemnite_int_id']);
             $table->foreign('indemnite_int_id')->references('id')->on('indemnite_intervention_types');
         });
     }
