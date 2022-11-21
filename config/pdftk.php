85180e3e1514e11ce2e6115632ed28500da74f34
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-11-21 20:39:31 +0100

    Fix pdftk config

--- config/pdftk.php
+++ config/pdftk.php
@@ -3,15 +3,14 @@
 return [
 
     /*
-     * You can enable CORS for 1 or multiple paths.
-     * Example: ['api/*']
+     * Configuration de PDFTK
      */
     'config' => env('PDFTK_BIN_PATH') ? array(
         // e.g. /project/pdftk/bin/pdftk
         'command' => env('PDFTK_BIN_PATH'),
         'procEnv' => array(
             // e.g. /project/pdftk/bin (should contain libgcj.so.10)
-            'LD_LIBRARY_PATH' => config('PDFTK_LIB_FOLDER')
+            'LD_LIBRARY_PATH' => env('PDFTK_LIB_FOLDER')
         ),
     ) : [],
 ];
