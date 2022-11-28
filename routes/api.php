74c701084fa6cb0fee4ce611dc2587ece8321328
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-11-28 15:02:43 +0100

    Adaptation des permissions pour module cours

--- routes/api.php
+++ routes/api.php
@@ -139,7 +139,7 @@ Route::group(['prefix' => 'v2', 'middleware' => [HttpLogger::class, DbSelector::
     });
 
     // Sapeurs
-    Route::group(['middleware' => 'jwtTokenRole:sapeur.lecture,exercice.lecture,intervention.lecture,organisation.modification,comptabilite.tout'], function () {
+    Route::group(['middleware' => 'jwtTokenRole:sapeur.lecture,exercice.lecture,intervention.lecture,organisation.modification,comptabilite.tout,cours.lecture'], function () {
         Route::resource('sapeurs', SapeurController::class)->only(['index', 'show']);
     });
 
