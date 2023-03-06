4d58c3a1862afd605fb75ec0514b363c6b52d0b6
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-03-06 16:39:47 +0100

    Annuler imputation annuel de ceux non imputé

--- app/Domaine/Business/ImputationBusiness.php
+++ app/Domaine/Business/ImputationBusiness.php
@@ -487,17 +487,18 @@ class ImputationBusiness
     public function annulerImputationAnnuel($exerciceComptableId)
     {
         // Check si des ecritures sont déjà liées à un décompte
-        if (Ecriture::where('exercice_comptable_id', '=', $exerciceComptableId)
-            ->where('module', '=', self::ECRITURE_MODULE_FRAIS_INDEMNITE_ANNUEL)
-            ->whereNotNull('decompte_id')
-            ->exists()
-        ) {
-            throw new ArrayException([], 'Des écriture sont déjà facturées dans un décompte.');
-        }
+        // if (Ecriture::where('exercice_comptable_id', '=', $exerciceComptableId)
+        //     ->where('module', '=', self::ECRITURE_MODULE_FRAIS_INDEMNITE_ANNUEL)
+        //     ->whereNotNull('decompte_id')
+        //     ->exists()
+        // ) {
+        //     throw new ArrayException([], 'Des écriture sont déjà facturées dans un décompte.');
+        // }
 
         // Suppression des écritures
         Ecriture::where('exercice_comptable_id', '=', $exerciceComptableId)
             ->where('module', '=', self::ECRITURE_MODULE_FRAIS_INDEMNITE_ANNUEL)
+            ->whereNull('decompte_id')
             ->delete();
 
         return true;
