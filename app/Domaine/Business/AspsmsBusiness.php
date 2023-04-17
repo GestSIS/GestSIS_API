6e0543496aca3795d8b4df3365c7026a8a8e48e5
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-04-17 15:40:57 +0200

    Revert sms convert to ascii

--- app/Domaine/Business/AspsmsBusiness.php
+++ app/Domaine/Business/AspsmsBusiness.php
@@ -23,28 +23,9 @@ class AspsmsBusiness
         return self::getParams();
     }
 
-    private static function utf8_to_gsm0338($string)
-    {
-        $dict = array(
-            '@' => "\x00", '£' => "\x01", '$' => "\x02", '¥' => "\x03", 'è' => "\x04", 'é' => "\x05", 'ù' => "\x06", 'ì' => "\x07", 'ò' => "\x08", 'Ç' => "\x09", 'Ø' => "\x0B", 'ø' => "\x0C", 'Å' => "\x0E", 'å' => "\x0F",
-            'Δ' => "\x10", '_' => "\x11", 'Φ' => "\x12", 'Γ' => "\x13", 'Λ' => "\x14", 'Ω' => "\x15", 'Π' => "\x16", 'Ψ' => "\x17", 'Σ' => "\x18", 'Θ' => "\x19", 'Ξ' => "\x1A", 'Æ' => "\x1C", 'æ' => "\x1D", 'ß' => "\x1E", 'É' => "\x1F",
-            // all \x2? removed
-            // all \x3? removed
-            // all \x4? removed
-            'Ä' => "\x5B", 'Ö' => "\x5C", 'Ñ' => "\x5D", 'Ü' => "\x5E", '§' => "\x5F",
-            '¿' => "\x60",
-            'ä' => "\x7B", 'ö' => "\x7C", 'ñ' => "\x7D", 'ü' => "\x7E", 'à' => "\x7F",
-            '^' => "\x1B\x14", '{' => "\x1B\x28", '}' => "\x1B\x29", '\\' => "\x1B\x2F", '[' => "\x1B\x3C", '~' => "\x1B\x3D", ']' => "\x1B\x3E", '|' => "\x1B\x40", '€' => "\x1B\x65"
-        );
-        $converted = strtr($string, $dict);
-
-        // Replace unconverted UTF-8 chars from codepages U+0080-U+07FF, U+0080-U+FFFF and U+010000-U+10FFFF with a single ?
-        return preg_replace('/([\\xC0-\\xDF].)|([\\xE0-\\xEF]..)|([\\xF0-\\xFF]...)/m', '?', $converted);
-    }
-
     public static function send($data)
     {
-        $message = self::utf8_to_gsm0338($data['message']);
+        $message = iconv('UTF-8', 'ASCII//TRANSLIT', $data['message']);
         $origin = "GestSIS"; // $data['origin']; // Pas pour le moment
         $differe = $data['differe'];
         $date = isset($data['date']) ? $data['date'] : "";
