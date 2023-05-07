513f48026d16af0d9edf9205e93efe9c21bc2d8c
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-05-07 14:50:45 +0200

    Fix convocation restreinte + update dependencies

--- app/Application/Http/Controllers/ConvocationController.php
+++ app/Application/Http/Controllers/ConvocationController.php
@@ -20,6 +20,16 @@ class ConvocationController extends Controller
 
     public function convoquer(Request $request, $exerciceComptableId)
     {
+        $request->merge([
+            'nip' => (bool) $request->get('nip', false),
+            'groupe' => (bool) $request->get('groupe', false),
+            'telephone' => (bool) $request->get('telephone', false),
+            'adresse' => (bool) $request->get('adresse', false),
+            'details' => (bool) $request->get('details', false),
+            'info' => (bool) $request->get('info', false),
+            'sapeurIds' => is_string($request->get('sapeurIds', '')) ? explode(',', $request->get('sapeurIds', '')) : $request->get('sapeurIds', ''),
+        ]);
+
         $data = $request->validate([
             'nip' => 'boolean',
             'groupe' => 'boolean',
@@ -31,9 +41,9 @@ class ConvocationController extends Controller
             'pourInfo' => 'string|nullable',
             'texteDebut' => 'string|nullable',
             'texteFin' => 'string|nullable',
+            'sapeurIds' => 'array|nullable',
             'sapeurIds.*' => 'integer'
         ]);
-
         return $this->service->convoquer($exerciceComptableId, $data);
     }
 }
