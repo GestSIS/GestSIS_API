ef764102737b4e8a56d11e025a3cf30ed00dcbee
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-07-24 17:39:59 +0200

    Ajout validation des absences

--- app/Domaine/Business/AbsenceBusiness.php
+++ app/Domaine/Business/AbsenceBusiness.php
@@ -3,13 +3,31 @@
 
 namespace App\Domaine\Business;
 
+use App\Domaine\Exceptions\ArrayException;
 use App\Infrastructure\Models\Absence;
+use Carbon\Carbon;
 
 class AbsenceBusiness
 {
 
+    private function checkOverlap($absenceId, $sapeurId, $absence): bool
+    {
+        return Absence::where([
+            ['id', '<>', $absenceId],
+            ['sapeur_id', '=', $sapeurId],
+            ['debut', '<=', $absence['fin']],
+            ['fin', '>=', $absence['debut']]
+        ])->exists();
+    }
     public function ajouterAbsence($data)
     {
+        if (Carbon::parse($data['debut'])->gt(Carbon::parse($data['fin']))) {
+            throw new ArrayException(['fin' => 'Invalide'], 'La date de fin ne peut être antérieur à la date de début !');
+        }
+
+        if ($this->checkOverlap(Null, $data['sapeur_id'], $data)) {
+            throw new ArrayException(['debut' => 'Invalide', 'fin' => 'Invalide'], 'Cette absence chevauche une autre absence');
+        }
         $absence = new Absence();
         $absence->fill($data);
         $absence->sapeur_id = $data['sapeur_id'];
@@ -19,6 +37,19 @@ class AbsenceBusiness
 
     public function modifierAbsence($absenceId, $data)
     {
+        if (Carbon::parse($data['debut'])->gt(Carbon::parse($data['fin']))) {
+            throw new ArrayException(['fin' => 'Invalide'], 'La date de fin ne peut être antérieur à la date de début !');
+        }
+
+        $absence = Absence::find($absenceId);
+        if (!$absence) {
+            throw new ArrayException([], 'Absence inexistante');
+        }
+
+        // Absence overlapping
+        if ($this->checkOverlap($absenceId, $absence->sapeur_id, $data)) {
+            throw new ArrayException(['debut' => 'Invalide', 'fin' => 'Invalide'], 'Cette absence chevauche une autre absence');
+        }
         // Chargement des absences
         Absence::where('id', $absenceId)->limit(1)->update($data);
         return Absence::find($absenceId);
