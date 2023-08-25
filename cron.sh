2c07b4cf1679246238620135d2a9a638996d77e7
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-08-25 20:29:13 +0200

    Upgrade cron to php8.2

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/opt/alt/php81/usr/bin/php /home/sishs/apis.gestsis.ch/artisan dbs:sapeurs-actif-status
+/opt/alt/php82/usr/bin/php /home/sishs/apis.gestsis.ch/artisan dbs:sapeurs-actif-status
 # php artisan schedule:run
