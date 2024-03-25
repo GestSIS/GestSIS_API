ba9bb01d3569fe01a5c9101ccd3a6b24743a009c
Bastien Wermeille <bastien.wermeille@gmail.com>
2024-03-25 18:42:44 +0100

    Revert "Migrate application structure to laravel 11 format"
    This reverts commit 9f72939c0b261836ee6ee98fd44d505f66d69863.

--- bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Application\Providers\AppServiceProvider::class,
-    App\Application\Providers\RepositoryServiceProvider::class,
-    App\Application\Providers\BusinessServiceProvider::class,
-];
