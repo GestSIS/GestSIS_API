3cbf0ea4c66200aeef241027f9ff22635e45a602
Bastien Wermeille <bastien.wermeille@gmail.com>
2024-03-29 20:38:03 +0100

    Fix sapeurs identifier

--- app/Domaine/API/EmailService.php
+++ app/Domaine/API/EmailService.php
@@ -24,12 +24,12 @@ class EmailService
         $dbs = config('database.dbs');
         $res = [];
         foreach ($dbs as $db) {
-            DB::reconnect($db);
+            DB::reconnect('db_' . $db);
             $sapeur = DB::connection($db)->table('sapeurs')->where('email', '=', $email)->select('sapeurs.id')->first();
             if (!is_null($sapeur)) {
                 $res[$db] = $sapeur->id;
             }
-            DB::disconnect($db);
+            DB::disconnect('db_' . $db);
         }
         return $res;
     }
