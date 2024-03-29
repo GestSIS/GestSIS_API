908ca1a1a35b429591b20d08203db5ff80e4e1d4
Bastien Wermeille <bastien.wermeille@gmail.com>
2024-03-29 20:40:24 +0100

    Format code

--- app/Domaine/API/EmailService.php
+++ app/Domaine/API/EmailService.php
@@ -25,7 +25,11 @@ class EmailService
         $res = [];
         foreach ($dbs as $db) {
             DB::reconnect('db_' . $db);
-            $sapeur = DB::connection('db_' . $db)->table('sapeurs')->where('email', '=', $email)->select('sapeurs.id')->first();
+            $sapeur = DB::connection('db_' . $db)
+                ->table('sapeurs')
+                ->where('email', '=', $email)
+                ->select('sapeurs.id')
+                ->first();
             if (!is_null($sapeur)) {
                 $res[$db] = $sapeur->id;
             }
