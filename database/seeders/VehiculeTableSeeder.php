3ef15475ac954e64f6e49a068e559a85c219e07e
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-04-25 15:27:51 +0200

    Improve seeders

--- database/seeders/VehiculeTableSeeder.php
+++ database/seeders/VehiculeTableSeeder.php
@@ -15,12 +15,12 @@ class VehiculeTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('emplacements')->insert([
-            ['id' => 5, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'Tonne-Pompe', 'tri' => 5, 'couleur_id' => 1, 'statut' => 1],
-            ['id' => 6, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'VPM', 'tri' => 6, 'couleur_id' => 1, 'statut' => 1],
-            ['id' => 7, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'Iveco', 'tri' => 7, 'couleur_id' => 1, 'statut' => 1],
-            ['id' => 8, 'parent_id' => 4, 'est_etiquete' => true, 'designation' => 'Mowag', 'tri' => 8, 'couleur_id' => 1, 'statut' => 1],
-            ['id' => 9, 'parent_id' => 3, 'est_etiquete' => true, 'designation' => 'VPI', 'tri' => 9, 'couleur_id' => 1, 'statut' => 1],
-            ['id' => 10, 'parent_id' => 3, 'est_etiquete' => true, 'designation' => 'Jeep', 'tri' => 10, 'couleur_id' => 1, 'statut' => 0],
+            ['id' => 5, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'Tonne-Pompe', 'tri' => 2, 'couleur_id' => 1, 'statut' => 1],
+            ['id' => 6, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'VPM', 'tri' => 3, 'couleur_id' => 1, 'statut' => 1],
+            ['id' => 7, 'parent_id' => 1, 'est_etiquete' => true, 'designation' => 'Iveco', 'tri' => 4, 'couleur_id' => 1, 'statut' => 1],
+            ['id' => 8, 'parent_id' => 4, 'est_etiquete' => true, 'designation' => 'Mowag', 'tri' => 6, 'couleur_id' => 1, 'statut' => 1],
+            ['id' => 9, 'parent_id' => 3, 'est_etiquete' => true, 'designation' => 'VPI', 'tri' => 8, 'couleur_id' => 1, 'statut' => 1],
+            ['id' => 10, 'parent_id' => 3, 'est_etiquete' => true, 'designation' => 'Jeep', 'tri' => 9, 'couleur_id' => 1, 'statut' => 0],
             ['id' => 11, 'parent_id' => 2, 'est_etiquete' => true, 'designation' => 'VPI', 'tri' => 11, 'couleur_id' => 1, 'statut' => 1],
             ['id' => 12, 'parent_id' => 2, 'est_etiquete' => true, 'designation' => 'Transport', 'tri' => 12, 'couleur_id' => 1, 'statut' => 1],
         ]);
