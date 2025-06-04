ea942edf88c540eeb451275c572fe6278d0b3f8d
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-06-04 21:52:24 +0000

    Rename pere_id en parent_id pour materiel categorie

--- database/migrations/2024_10_09_111825_update_groupe_table.php
+++ database/migrations/2024_10_09_111825_update_groupe_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,6 +12,12 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::table('materiel_categories', function (Blueprint $table) {
+            $table->dropForeign('materiel_categories_pere_id_foreign');
+            $table->renameColumn('pere_id', 'parent_id');
+
+            $table->foreign('parent_id')->references('id')->on('materiel_categories')->onDelete('cascade');
+        });
         Schema::table('groupes', function (Blueprint $table) {
             $table->dropForeign('groupes_pere_id_foreign');
             $table->renameColumn('pere_id', 'parent_id');
@@ -34,6 +39,12 @@ return new class extends Migration
             $table->renameColumn('parent_id', 'pere_id');
 
             $table->foreign('pere_id')->references('id')->on('groupes')->onDelete('cascade');
+        });
+        Schema::table('materiel_categories', function (Blueprint $table) {
+            $table->dropForeign('materiel_categories_parent_id_foreign');
+            $table->renameColumn('parent_id', 'pere_id');
+
+            $table->foreign('pere_id')->references('id')->on('materiel_categories')->onDelete('cascade');
         });
     }
 };
