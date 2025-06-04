43ed116e4ff3d4b9346bcf0dcf070b3ff4695c64
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-06-04 20:13:56 +0200

    Fix migration

---
+++ app/Infrastructure/Models/MaterielGenerique.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaterielGenerique extends Model
+{
+}
