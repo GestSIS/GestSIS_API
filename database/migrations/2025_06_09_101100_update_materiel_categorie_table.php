ee76f49b12448ceff601195c0eb6b970563725c5
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-06-10 18:14:50 +0000

    Clean DB

---
+++ database/migrations/2025_06_09_101100_update_materiel_categorie_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('materiel_alerte_type_pour');
+        Schema::dropIfExists('materiel_alerte_types');
+        Schema::dropIfExists('materiel_event_type_pour');
+        Schema::dropIfExists('materiel_events');
+        Schema::dropIfExists('materiel_event_types');
+        Schema::dropIfExists('materiel_alertes');
+
+        Schema::dropIfExists('materiel_nominals');
+        Schema::dropIfExists('materiel_generiques');
+        Schema::dropIfExists('materiel_personnels');
+
+        Schema::dropIfExists('vehicules');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+};
