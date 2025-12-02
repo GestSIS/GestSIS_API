77bed206ab55df47cbd4c53111077fdf4e7fd1fb
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-12-02 08:54:52 +0000

    Temp migration rta

--- app/Domaine/API/RtaService.php
+++ app/Domaine/API/RtaService.php
@@ -49,6 +49,15 @@ class RtaService
 
     public function getReferenceRta()
     {
+        $data = ReferenceRta::all()->toArray();
+        return array_map(function ($s) {
+            $data = json_decode($s['data'], true);
+            unset($s['data']);
+            $s = $s + $data;
+            return $s;
+        }, $data);
+
+        // TODO: Temp migration for RTA
         $params = RtaParam::first();
         if (!$params) {
             throw new ArrayException(['message' => 'Paramètres RTA invalides'], 'Paramètres RTA invalides');
