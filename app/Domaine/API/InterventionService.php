1829185acb14a48cf13623802b7ee4854df80c1d
bastien wermeille <bastien.wermeille@gmail.com>
2026-03-05 20:18:41 +0000

    Fix rapport inter

--- app/Domaine/API/InterventionService.php
+++ app/Domaine/API/InterventionService.php
@@ -462,7 +462,7 @@ class InterventionService
         if (in_array('presences', $withOptions) || in_array('presencesResume', $withOptions)) {
             $sapeurs = Sapeur::get(['nom', 'prenom', 'id'])->toArray();
             foreach ($sapeurs as $sapeur) {
-                $sapeursMap[$sapeur->id] = $sapeur;
+                $sapeursMap[$sapeur->id] = $sapeur->toArray();
             }
 
             foreach ($intervention->presences as $presence) {
